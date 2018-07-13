0bea45e049561a39de2ff3c1b2d195499bbd87d6
mayaflor <maya.r.rebelo@gmail.com>
2018-07-13 16:24:46 -0300

    gráfico teste

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-/*
- * Funcionalidad de tu producto
- */
+// gráfico rosquinha de alunas presentes e desistentes na Laboratória 
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work',     11],
+    ['Eat',      2],
+    ['Commute',  2],
+    ['Watch TV', 2],
+    ['Sleep',    7]
+  ]);
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
+  var options = {
+    title: 'My Daily Activities',
+    pieHole: 0.4,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  chart.draw(data, options);
+}
+
+
 console.log(data);
