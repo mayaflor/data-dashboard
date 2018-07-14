4958df013fe54a13b42d174a45d39440e90e4124
mayaflor <maya.r.rebelo@gmail.com>
2018-07-14 16:59:08 -0300

    primeiros gráficos adicionados

--- js/app.js
+++ js/app.js
@@ -1,38 +1,67 @@
+// --------------AQP-------------
 
-  for (sede in data){ 
-    for ( turma in sede){
-      for (students in turma)
-      for (i in data[sede][turma][students]){
-        var active = data[sede][turma][students][2]
-      }
-    }
+
+// ativas e desativas em AQP
+var activeTrueAQP = 0;
+var activeFalseAQP = 0;
+for ( turma in data['AQP'])
+for ( i in data['AQP'][turma]['students']) {
+  if (data['AQP'][turma]['students'][i]['active'] == true) {
+    activeTrueAQP++;
+  } else if (data['AQP'][turma]['students'][i]['active'] == false) {
+    activeFalseAQP++;
   }
-
-console.log(active);
-
-
-
-
-    
-// gráfico rosquinha de alunas presentes e desistentes na Laboratória 
+  var totalActiveAQP = activeTrueAQP + activeFalseAQP;
+}
+// gráfico ativas e desativas em AQP
 google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work',11],
-    ['Eat',      2],
-    ['Commute',  2],
-    ['Watch TV', 2],
-    ['Sleep',    7]
+google.charts.setOnLoadCallback(donutchartActiveAQP);
+function donutchartActiveAQP() {
+  var chartData = google.visualization.arrayToDataTable([
+    ['',''],
+    ['presentes',activeTrueAQP],
+    ['desistentes',activeFalseAQP],
   ]);
-
   var options = {
-    title: 'My Daily Activities',
+    title: 'Número de alunas presentes e desistentes em Arequipa',
     pieHole: 0.4,
   };
+  var chart = new google.visualization.PieChart(document.getElementById('donutchartActiveAQP'));
+  chart.draw(chartData, options);
+}
 
-  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-  chart.draw(data, options);}
 
-  console.log(data);
+
+
+
+// --------------CDMX-------------
+
+
+// ativas e desativas em CDMX
+var activeTrueCDMX = 0;
+var activeFalseCDMX = 0;
+for ( turma in data['CDMX'])
+for ( i in data['CDMX'][turma]['students']) {
+  if (data['CDMX'][turma]['students'][i]['active'] == true) {
+    activeTrueCDMX++;
+  } else if (data['CDMX'][turma]['students'][i]['active'] == false) {
+    activeFalseCDMX++;
+  }
+  var totalActiveCDMX = activeTrueCDMX + activeFalseCDMX;
+}
+// gráfico ativas e desativas em AQP
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(donutchartActiveCDMX);
+function donutchartActiveCDMX() {
+  var chartData = google.visualization.arrayToDataTable([
+    ['',''],
+    ['presentes',activeTrueCDMX],
+    ['desistentes',activeFalseCDMX],
+  ]);
+  var options = {
+    title: 'Número de alunas presentes e desistentes na Cidade do México',
+    pieHole: 0.4,
+  };
+  var chart = new google.visualization.PieChart(document.getElementById('donutchartActiveCDMX'));
+  chart.draw(chartData, options);
+}
