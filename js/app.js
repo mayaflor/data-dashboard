34a0573602ff2179100605fe2ba78495a5ad3ee1
mayaflor <maya.r.rebelo@gmail.com>
2018-07-14 17:24:06 -0300

    gráfico alunas presentes e desistentes nas sedes prontos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // --------------AQP-------------
 
 
-// ativas e desativas em AQP
+// alunas presentes e desistentes em AQP
 var activeTrueAQP = 0;
 var activeFalseAQP = 0;
 for ( turma in data['AQP'])
@@ -13,7 +13,7 @@ for ( i in data['AQP'][turma]['students']) {
   }
   var totalActiveAQP = activeTrueAQP + activeFalseAQP;
 }
-// gráfico ativas e desativas em AQP
+// gráfico alunas presentes e desistentes em AQP
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(donutchartActiveAQP);
 function donutchartActiveAQP() {
@@ -37,7 +37,7 @@ function donutchartActiveAQP() {
 // --------------CDMX-------------
 
 
-// ativas e desativas em CDMX
+// alunas presentes e desistentes em CDMX
 var activeTrueCDMX = 0;
 var activeFalseCDMX = 0;
 for ( turma in data['CDMX'])
@@ -49,7 +49,7 @@ for ( i in data['CDMX'][turma]['students']) {
   }
   var totalActiveCDMX = activeTrueCDMX + activeFalseCDMX;
 }
-// gráfico ativas e desativas em AQP
+// gráfico alunas presentes e desistentes em CDMX
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(donutchartActiveCDMX);
 function donutchartActiveCDMX() {
@@ -63,5 +63,76 @@ function donutchartActiveCDMX() {
     pieHole: 0.4,
   };
   var chart = new google.visualization.PieChart(document.getElementById('donutchartActiveCDMX'));
+  chart.draw(chartData, options);
+}
+
+
+
+
+
+// --------------LIM-------------
+
+
+// alunas presentes e desistentes em LIM
+var activeTrueLIM = 0;
+var activeFalseLIM = 0;
+for ( turma in data['LIM'])
+for ( i in data['LIM'][turma]['students']) {
+  if (data['LIM'][turma]['students'][i]['active'] == true) {
+    activeTrueLIM++;
+  } else if (data['LIM'][turma]['students'][i]['active'] == false) {
+    activeFalseLIM++;
+  }
+  var totalActiveLIM = activeTrueLIM + activeFalseLIM;
+}
+// gráfico alunas presentes e desistentes em LIM
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(donutchartActiveLIM);
+function donutchartActiveLIM() {
+  var chartData = google.visualization.arrayToDataTable([
+    ['',''],
+    ['presentes',activeTrueLIM],
+    ['desistentes',activeFalseLIM],
+  ]);
+  var options = {
+    title: 'Número de alunas presentes e desistentes em Lima',
+    pieHole: 0.4,
+  };
+  var chart = new google.visualization.PieChart(document.getElementById('donutchartActiveLIM'));
+  chart.draw(chartData, options);
+}
+
+
+
+
+// --------------SCL-------------
+
+
+// alunas presentes e desistentes em SCL
+var activeTrueSCL = 0;
+var activeFalseSCL = 0;
+for ( turma in data['SCL'])
+for ( i in data['SCL'][turma]['students']) {
+  if (data['SCL'][turma]['students'][i]['active'] == true) {
+    activeTrueSCL++;
+  } else if (data['SCL'][turma]['students'][i]['active'] == false) {
+    activeFalseSCL++;
+  }
+  var totalActiveSCL = activeTrueSCL + activeFalseSCL;
+}
+// gráfico alunas presentes e desistentes em SCL
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(donutchartActiveSCL);
+function donutchartActiveSCL() {
+  var chartData = google.visualization.arrayToDataTable([
+    ['',''],
+    ['presentes',activeTrueSCL],
+    ['desistentes',activeFalseSCL],
+  ]);
+  var options = {
+    title: 'Número de alunas presentes e desistentes em Santiago',
+    pieHole: 0.4,
+  };
+  var chart = new google.visualization.PieChart(document.getElementById('donutchartActiveSCL'));
   chart.draw(chartData, options);
 }
