8a5876182b28fef9e3b9c010061e8fdd7f76d6ac
mayaflor <maya.r.rebelo@gmail.com>
2018-07-17 17:00:55 -0300

    início de menus drop down

--- js/app.js
+++ js/app.js
@@ -2117,3 +2117,14 @@ for (turma in data['SCL']) {
     imgSCL.width = 100;
   }
 }
+
+
+
+// ********************DROP DOWN MENU***********************
+var dropDownValue = document.getElementById("dropDown");
+
+dropDownValue.onchange = function() {
+  if (this.selectedIndex !== 0) {
+    window.location.href = this.value;
+  }
+};
